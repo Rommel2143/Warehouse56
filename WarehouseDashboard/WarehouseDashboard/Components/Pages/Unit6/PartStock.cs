75a0e02230c2d3872ace2dbe5402dad4cd8a05a9
R. Magnaye <164116444+Rommel2143@users.noreply.github.com>
2026-09-03 15:18:36 +0800

    1

---
+++ WarehouseDashboard/WarehouseDashboard/Components/Pages/Unit6/PartStock.cs
@@ -0,0 +1,14 @@
+﻿namespace WarehouseDashboard.Components.Pages.Unit6
+{
+    public sealed class PartStock
+    {
+        public string? PartCode { get; set; }
+
+        public string? PartName { get; set; }
+
+        public int Stock { get; set; }
+        public int Boxes { get; set; }
+        public int StockIn { get; internal set; }
+        public int StockOut { get; internal set; }
+    }
+}
